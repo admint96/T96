2ab2b37d46e83536ecb6a685d0f480b221de5939
admint96 <admin@talent96.com>
2025-07-04 12:39:34 +0530

    Update Auth.js

--- Backend/Routes/Auth.js
+++ Backend/Routes/Auth.js
@@ -101,8 +101,7 @@ router.post('/login', async (req, res) => {
     
     const token = jwt.sign(
       { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: '7d' }
+      process.env.JWT_SECRET
     );
 
     
