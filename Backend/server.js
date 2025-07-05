1cb45b8e7a3f253d75cac55cb015528b71495082
admint96 <admin@talent96.com>
2025-07-05 15:58:57 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('Socket disconnected:', socket.id);
+    console.log(' Socket disconnected:', socket.id);
   });
 });
 
@@ -47,15 +47,22 @@ app.use((req, res, next) => {
 });
 
 
+
+app.get("/status", (req, res) => {
+  res.status(500).json({ message: "Server error" });
+});
+
+
+
+
 const authRoutes = require('./Routes/Auth');
 const userRoutes = require('./Routes/userRoutes');
 const uploadRoutes = require('./Routes/UploadProfile');
 const recuriterRoutes = require('./Routes/recuriterRoutes');
 const verifyRoutes = require('./Routes/verifyRoutes');
-
+const activityRoutes=require('./Routes/ActivityRoutes')
 
 const notificationRoutes = require('./Routes/notificationRoutes')(io);
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
@@ -65,6 +72,8 @@ app.use('/api/verify', verifyRoutes);
 app.use('/api/notifications', notificationRoutes);
 
 
+app.use('/api/activities', activityRoutes);
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -72,9 +81,9 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => {
   console.log(' MongoDB connected');
 
-
+  
   server.listen(process.env.PORT || 5000, '0.0.0.0', () => {
-    console.log(` Server running on port ${process.env.PORT || 5000}`);
+    console.log(`Server running on port ${process.env.PORT || 5000}`);
   });
 })
 .catch(err => {
